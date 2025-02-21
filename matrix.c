d64d8d086e09b1f153594b6bfce06bafeec2a9a9
Valentino Glave <defliez@protonmail.com>
2025-02-21 22:48:23 +0100

    refactor thread creation logic and color initialization

--- matrix.c
+++ matrix.c
@@ -12,7 +12,6 @@ void* printRandomNumber(void* arg) {
     free(arg);
     int randomNum;
     int height, width;
-    getmaxyx(stdscr, height, width);
 
     int y = 0;
 
@@ -20,6 +19,7 @@ void* printRandomNumber(void* arg) {
     usleep(randomOffset * 500000);
 
     while (1) {
+    getmaxyx(stdscr, height, width);
         randomNum = rand() % 10;
 
         pthread_mutex_lock(&lock);
@@ -40,7 +40,7 @@ void* printRandomNumber(void* arg) {
 
 
 
-void* printWakeUp() {
+void* printWakeUp(void* arg) {
     int height, width;
     getmaxyx(stdscr, height, width);
     int centerY = height / 2;
@@ -59,13 +59,17 @@ int main() {
     pthread_mutex_init(&lock, NULL);
 
     start_color();
-    init_pair(1, COLOR_GREEN, COLOR_BLACK);
+    use_default_colors();
+    init_pair(1, COLOR_GREEN, -1);
     attron(COLOR_PAIR(1));
     attron(A_BOLD);
 
-    pthread_t threads[140];
+    int width = getmaxx(stdscr);
+    int threadCnt = width / 3;
 
-    for (int i = 0; i < 140; i++) {
+    pthread_t threads[threadCnt];
+
+    for (int i = 0; i <= threadCnt; i++) {
         int* xCoord = malloc(sizeof(int));
         *xCoord = i * 3;
         pthread_create(&threads[i], NULL, printRandomNumber, (void*)xCoord);
@@ -75,8 +79,7 @@ int main() {
 
     while (1) {
         usleep(100000);
-
-        if (screenFilledThreads >= 140) {
+        if (screenFilledThreads >= threadCnt) {
             pthread_create(&threadWakeUp, NULL, printWakeUp, NULL);
         }
     }
